c7f779ec3a0133c6405fa092de36d10d40529257
Mirsandev <xmirandadevx@gmail.com>
2023-12-07 22:37:09 -0400

    tipos de datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,22 @@
 <?php include 'includes/header.php';
 
+// Boolean
+$logeado = true;
+var_dump($logeado);
 
+// Enteros
+$numero = 200;
+var_dump($numero);
 
+// Floats
+$float = 200.5;
+var_dump($float);
+
+// strings
+$nombre + "juan";
+var_dump($nombre);
+
+$array = [];
+var_dump($array);
 
 include 'includes/footer.php';
