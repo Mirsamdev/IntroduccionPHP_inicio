d89982897b54c97abfdb87d8c2250ec0f4dc66b2
Mirsandev <xmirandadevx@gmail.com>
2023-12-08 07:53:10 -0400

    Strings

--- 07-string-methods.php
+++ 07-string-methods.php
@@ -1,6 +1,36 @@
 <?php include 'includes/header.php';
 
+$nombreCliente = "Obed Miranda";
 
+// Conocer extension de un string
+echo strlen($nombreCliente);
+var_dump($nombreCliente);
+
+// Eliminar espacios en blanco 
+$texto = trim($nombreCliente);
+echo strlen($texto);
+
+//  Convertirlo a mayusculas
+echo strtoupper($nombreCliente);
+
+// Minusculas
+echo strtolower($nombreCliente);
+
+$mail1 = "correo@correo.com";
+$mail2 = "Correo@correo.com";
+
+var_dump(strtolower($mail1) === strtolower($mail2));
+
+echo str_replace('Obed','O', $nombreCliente);
+
+// Revisar si un string existe o no
+echo strpos($nombreCliente, 'Obed');
+
+$tipoCliente = "Premium";
+
+echo "El Cliente " . $nombreCliente . " es " . $tipoCliente;
+
+echo "El Cliente {$nombreCliente} es {$tipoCliente}";
 
 
 include 'includes/footer.php';
