2c269c8642ca65c937a65d0ab8133dc68230c109
Mirsandev <xmirandadevx@gmail.com>
2023-12-08 08:23:34 -0400

    associative Arrays

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,14 +1,29 @@
 <?php include 'includes/header.php';
 
-$cliente = [
+$clientes = [
   'nombre' => 'Juan',
   'saldo' => 200,
   'informacion' => [
-    'tipo' => 'premium'
-
+    'tipo' => 'premium',
+    'disponible' => 100
   ]
   ];
 
+  echo "<pre>";
+var_dump($clientes['nombre']);
+echo "</pre>";
+
+// echo $clientes['nombre'];
+// echo $clientes['informacion']['tipo'];
+// echo $clientes['informacion']['disponible'];
+
+$clientes['codigo'] = 1209102012;
+echo $clientes['informacion']['tipo'];
+
+echo "<pre>";
+var_dump($clientes);
+echo "</pre>";
 
 
 include 'includes/footer.php';
+
