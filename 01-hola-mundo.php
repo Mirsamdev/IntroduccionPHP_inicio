b1c1d0387960f5f5289336b4eb0d344dcd27fab8
Mirsandev <xmirandadevx@gmail.com>
2023-12-07 22:31:47 -0400

    PHP Learn

---
+++ 01-hola-mundo.php
@@ -0,0 +1,15 @@
+<?php include 'includes/header.php';
+
+
+// echo "HOLA MUNDO";
+// echo ('Hola mundo')
+
+// print 'Hola Mundo';
+
+// print('Hola mundo');
+
+// print_r('Hola mundo');
+
+// var_dump("Hola mundo");
+
+include 'includes/footer.php';
---
+++ 02-variables.php
@@ -0,0 +1,22 @@
+<?php include 'includes/header.php';
+
+$nombre = " Monza";
+
+
+
+echo $nombre;
+
+var_dump($nombre);
+
+define('constante', "este es el valor de la constante");
+
+echo constante;
+
+const constante2 = "HOLA 2";
+
+echo constante2;
+
+$nombreCliente = "Pedro";
+$nombre_cliente + "Pedro";
+
+include 'includes/footer.php';
