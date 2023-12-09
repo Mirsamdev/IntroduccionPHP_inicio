50e8deeaca69590f8b147bea5275f10aaaa26b3d
Mirsandev <xmirandadevx@gmail.com>
2023-12-09 12:28:03 -0400

    aleluya :v tipos de sort

--- 11-funciones-arrays.php
+++ 11-funciones-arrays.php
@@ -7,13 +7,33 @@ var_dump( in_array('Tablet', $carrito) );
 var_dump( in_array('Audifonos', $carrito) );
 
 // Ordenar elementos de un arreglo
-$numeros = array(1,3,4,5,1,2);
+$numeros = array(1,3,4,5,1,2); // de menor a mayor
+rsort($numeros); // de mayor a menor
 sort($numeros); 
 
 echo "<pre>";
 var_dump($numeros);
 echo "</pre>";
 
+// Ordenar arreglo asociativo
+$cliente = array(
+  'saldo' => 200,
+  'tipo' => 'Premium',
+  'nombre' => 'Juan'
+);
 
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+asort($cliente); // Ordena por valores (orden alfabetico)
+arsort($cliente); // Ordena por valores, Z primero
+ksort($cliente); // Ordena por llaves (orden alfabetico)
+krsort($cliente); // Ordena por llaves (orden alfabetico, de la Z a la A)
+
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
