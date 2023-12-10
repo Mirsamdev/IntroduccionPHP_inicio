a15e87dcc4c5fffacf3e35f18174d5006c8a6051
Mirsandev <xmirandadevx@gmail.com>
2023-12-10 10:24:26 -0400

    for loop

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -15,9 +15,43 @@ if($autenticado || $admin) {
 $cliente = [
   'nombre' => 'Monza',
   'saldo' => 200,
-  'informacion =>' [
-      'tipo' => 'Premium';
-  ]
+  'informacion' => [
+      'tipo' => 'Premium'
+      ]
   ];
+
+  if( !empty($cliente)) {
+    echo "El arreglo de cliente esta vacio";
+
+    if($cliente['saldo'] > 0) {
+      echo "El saldo del cliente esta disponible";
+    } else {
+      echo "El saldo del cliente no esta disponible";
+    }
+  } 
+
+
+// else if
+if($cliente['saldo'] > 0) {
+  echo "el cliente tiene plata";
+} else if ($liente['informacion'] ['tipo'] === 'Premium') {
+  echo "El cliente es Premium";
+} else {
+  echo " no hay cliente definido o no tiene saldo o no es premium";
+}
+
+// Switch
+$tecnologia = 'PHP';
+
+switch ($tecnologia) {
+  case 'PHP':
+    echo "PHP, un exelente lenguaje!";
+    break;
+
+    default:
+    echo "No lo conozco papa";
+    break;
+}
+
 
 include 'includes/footer.php';
--- 13-loops.php
+++ 13-loops.php
@@ -1,5 +1,72 @@
 <?php include 'includes/header.php';
 
+// While 
+
+// $i = 0;
+
+// while($i <10) {
+
+//   echo $i . "<br>";
+
+//   $i++; // Incremento
+// }
+
+// echo $i . "<br>";
+
+// // Do while
+// $i = 0;
+
+// do {
+//   echo $i . "<br>";
+
+//   $i++;
+// } while($i < 10);
+
+
+
+
+// For loop.
+// for($i = 0;$i < 10; $i++ ) {
+//   if($i % 3 === 0) {
+//     echo $i . " - fizz ";
+//   }
+// };
+
+// For each 
+$clientes = array('Pedro', 'Juan', 'Karen');
+
+foreach( $clientes as $cliente ) {
+  echo $cliente . "<br>";
+}
+
+for($i = 0; $i < count($clientes); $i++) {
+echo $clientes[$i] . '<br/>';
+}
+
+$prueba = [
+  'nombre' => 'Samuel',
+  'saldo' => '200',
+  'tipo' => 'Premium'
+];
+
+foreach( $prueba as $key => $valor ):
+  echo $key . " - " $valor . "<br>";
+endforeach;
+
+
+
+
+
+// // Ejercicios
+// $pruebas = array('monza', 'prueba', 'foreach');
+
+// foreach( $pruebas as $prueba) {
+//   echo $cliente . "<br>";
+// }
+
+// for($i = 0; $i < count($pruebas); $i++) {
+//   echo $pruebas[$i] . '<br/>';
+// }
 
 
 
