fcd5daff882530fc4580a4f8a7b3b4f03dbebc11
Mirsandev <xmirandadevx@gmail.com>
2023-12-09 20:30:41 -0400

    cliente

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,7 +1,23 @@
 <?php include 'includes/header.php';
 
 
+$autenticado = true;
+$admin = false;
+
+if($autenticado || $admin) {
+  echo "usuario autenticado correctamente";
+} else {
+  echo "usuario no autenticado, inicia sesion";
+}
 
 
+// If anidados..
+$cliente = [
+  'nombre' => 'Monza',
+  'saldo' => 200,
+  'informacion =>' [
+      'tipo' => 'Premium';
+  ]
+  ];
 
 include 'includes/footer.php';
